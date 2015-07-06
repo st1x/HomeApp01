2983cdb0d1a6f885490475b51be18bef7800aad5
st1x <servis.2012@yandex.ru>
2015-07-06 15:11:30 +0300

    Initial commit

---
+++ app/src/main/java/com/example/admin/homeapp01/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.admin.homeapp01;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+    Button button1;
+    Button button2;
+    Toast toast;
+    ImageView imageView;
+    LinearLayout toastImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ImageView imageView = (ImageView) findViewById(R.id.image);
+        imageView.setImageDrawable(getResources().getDrawable(R.drawable.ic_brightness_5_black_48dp));
+
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.button1:
+                        imageView.setImageDrawable(getResources().getDrawable(R.drawable.ic_brightness_5_black_48dp));
+                        toast = Toast.makeText(MainActivity.this, "Выбрана картинка номер 1", Toast.LENGTH_SHORT);
+                        toast.show();
+                        break;
+                    case R.id.button2:
+                        imageView.setImageDrawable(getResources().getDrawable(R.drawable.ic_brightness_6_black_48dp));
+                        toast = Toast.makeText(MainActivity.this, "Выбрана картинка номер 2", Toast.LENGTH_SHORT);
+                        toast.show();
+                        break;
+                    }
+
+            }
+        };
+
+        button1.setOnClickListener(onClickListener);
+        button2.setOnClickListener(onClickListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
